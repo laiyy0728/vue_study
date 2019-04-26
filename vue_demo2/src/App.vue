3712b5530fd6509a9fe091357a9bb5aff9ff2e28
laiyy0728 <laiyy0728@gmail.com>
2019-04-26 12:03:19 +0800

    vue 组件间通信交互

--- vue_demo2/src/App.vue
+++ vue_demo2/src/App.vue
@@ -1,21 +1,56 @@
 <template>
   <div>
-    <img class="logo" src="./assets/logo.png" alt="logo">
-<!--  3、使用组件标签  -->
-    <HelloWorld/>
+    <header class="site-header jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <h1>请发表对 Vue 的评论</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+
+<!-- 组件间通信 -->
+    <Add :addComment="addComment"></Add>
+    <List :comments="comments" :delComment="delComment"></List>
   </div>
 </template>
 
 
 <script>
   // 1、引入组件
-  import HelloWorld from './components/HelloWorld.vue'
+  import Add from './components/Add.vue'
+  import List from './components/List.vue'
+
   export default {
-    // 2、映射组件标签
+
+    data() {
+      return {
+        // 数据在哪个组件，操作数据的行为（方法、函数）就应该定义在哪个组件
+        comments: [
+          {name: '张三', content: 'Vue 哈哈哈'},
+          {name: '李四', content: 'Vue 嘿嘿嘿'},
+          {name: '王五', content: 'Vue哟哟哟'},
+        ]
+      }
+    },
+
+    methods: {
+      addComment(comment){
+        this.comments.unshift(comment)
+      },
+      delComment(index) {
+        this.comments.splice(index, 1)
+      }
+    },
+
     components: {
-      HelloWorld
+      Add,
+      List
     }
   }
+
+
 </script>
 
 <style>
